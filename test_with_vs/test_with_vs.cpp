5318fd2261521df4e22cd48d4a51914bb718dee9
jun <2628059276@qq.com>
2018-08-08 15:03:20 +0800

    添加项目文件。

---
+++ test_with_vs/test_with_vs.cpp
@@ -0,0 +1,13 @@
+// test_with_vs.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "shit shit smell beautiful!" << endl;
+    return 0;
+}
+
